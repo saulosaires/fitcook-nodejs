a8f77de7ab13a3bb12d070bd5df2cca3a9c6d4bd
saulosaires <saulosaires@gmail.com>
2015-09-09 01:24:54 -0300

    's'

--- server.js
+++ server.js
@@ -21,36 +21,7 @@ mean.serve({ /*options placeholder*/ }, function(app, config) {
     
  var pathname = url.parse(req.url).pathname;
 		 
-		switch(pathname){
 		
-			case '/api/':
-				res.write('nada por enquanto');
-				res.end();
-			break;				
-
-			case '/api/userLogin':
-				user.login(req,res);
-			break;	
-
-			case '/api/userRetrieve':
-				user.retrieve(req,res);
-			break;	
-			
-			case '/api/groupCreate':
-				group.create(req,res);
-			break;	
-			
-			case '/api/groupRetrieve':
-				group.retrieve(req,res);
-			break;	
- 
-			
-			default:
-				res.write('default');
-				res.end();
-			break;				
-			
-		}	
 	
 	
   });
