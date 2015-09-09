3664311bb3caf24cc1575ac950ec656abf227c19
saulosaires <saulosaires@gmail.com>
2015-09-09 01:41:54 -0300

    's'

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ var group=require('./group/index');
 
 // Creates and serves mean application
 mean.serve({ /*options placeholder*/ }, function(app, config) {
+
   console.log('Mean app started on port ' + config.http.port + ' (' + process.env.NODE_ENV + ')');
   if(config.https && config.https.port){
     console.log('Mean secure app started on port ' + config.https.port + ' (' + process.env.NODE_ENV + ')');
@@ -17,11 +18,40 @@ mean.serve({ /*options placeholder*/ }, function(app, config) {
   
   global.urlMongo='mongodb://admin:NlCv6mWKfga1@127.10.56.2:27017/mean';
   
-  app.get('/api/*'', function (req,res,next) {
+  app.get('/api/*', function (req,res,next) {
     
  var pathname = url.parse(req.url).pathname;
 		 
+		switch(pathname){
 		
+			case '/api/':
+				res.write('nada por enquanto');
+				res.end();
+			break;				
+
+			case '/api/userLogin':
+				user.login(req,res);
+			break;	
+
+			case '/api/userRetrieve':
+				user.retrieve(req,res);
+			break;	
+			
+			case '/api/groupCreate':
+				group.create(req,res);
+			break;	
+			
+			case '/api/groupRetrieve':
+				group.retrieve(req,res);
+			break;	
+ 
+			
+			default:
+				res.write('default');
+				res.end();
+			break;				
+			
+		}	
 	
 	
   });
