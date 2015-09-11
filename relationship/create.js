583ce25d36fe58ad6662d8cefbb960e61b578d86
saulosaires <saulosaires@gmail.com>
2015-09-11 01:37:34 -0300

    's'

--- relationship/create.js
+++ relationship/create.js
@@ -6,25 +6,25 @@ exports.create = function(req,res) {
 	  var user_2 = req.param("user_2");
 	    
 	  if(typeof user_1 == 'undefined' || user_1 === null || user_1 === "null" ){
-		res.write('{status:error,msg:user_1_invalid}');res.end();
+		res.write('{status:error,msg:user_1_invalid}');res.end();return;
 	  }
  
 	  if(exports.query_profile(user_1)==0){
-	    res.write('{status:error,msg:user_1_not_exist}');res.end();
+	    res.write('{status:error,msg:user_1_not_exist}');res.end();return;
 	  }
 	  
 	  
 	  
 	  if(typeof user_2 == 'undefined' || user_2 === null || user_2 === "null" ){
-		res.write('{status:error,msg:user_2_invalid}');res.end();
+		res.write('{status:error,msg:user_2_invalid}');res.end();return;
 	  }
 
 	  if(exports.query_profile(user_2)==0){
-	    res.write('{status:error,msg:user_2_not_exist}');res.end();
+	    res.write('{status:error,msg:user_2_not_exist}');res.end();return;
 	  }
 
 	  if( user_1 == user_2 ){
-		res.write('{status:error,msg:user_1_and_user_2_eq}');res.end();
+		res.write('{status:error,msg:user_1_and_user_2_eq}');res.end();return;
 	  }
 
 	  
