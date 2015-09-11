13f88fb97023af9a4b468bb1a8b5edc87363c10e
saulosaires <saulosaires@gmail.com>
2015-09-11 03:09:47 -0300

    's'

--- relationship/respond.js
+++ relationship/respond.js
@@ -9,9 +9,12 @@ exports.respond = function(req,res) {
 		res.write('{status:error,msg:uuid_invalid}');res.end();return;
 	  }
  
-	  if(typeof respond == 'undefined' || respond == null || respond == "null" || 
-	            respond!='y'           || respond!='n'    || respond!='Y'      || respond!='N'){
+	  if(typeof respond == 'undefined' || respond == null || respond == "null" ){
 		res.write('{status:error,msg:respond_invalid}');res.end();return;
+	  }
+	  
+	  if(respond!='y' && respond!='n' && respond!='Y' && respond!='N'){
+		res.write('{status:error,msg:respond_invalid2}');res.end();return;
 	  }
 
 	 res.write('wqqwe');res.end();return;
