6e54a5a52e3687bf6f188af88111438ead549c86
saulosaires <saulosaires@gmail.com>
2015-09-11 02:20:34 -0300

    's'

---
+++ relationship/send.js
@@ -0,0 +1,81 @@
+var uuid = require('node-uuid');
+
+exports.send = function(req,res) {
+	
+	  var user_1 = req.param("user_1");
+	  var user_2 = req.param("user_2");
+	    
+	  if(typeof user_1 == 'undefined' || user_1 === null || user_1 === "null" ){
+		res.write('{status:error,msg:user_1_invalid}');res.end();return;
+	  }
+ 
+	  if(exports.query_profile(user_1)==0){
+	    res.write('{status:error,msg:user_1_not_exist}');res.end();return;
+	  }
+	  
+	  
+	  
+	  if(typeof user_2 == 'undefined' || user_2 === null || user_2 === "null" ){
+		res.write('{status:error,msg:user_2_invalid}');res.end();return;
+	  }
+
+	  if(exports.query_profile(user_2)==0){
+	    res.write('{status:error,msg:user_2_not_exist}');res.end();return;
+	  }
+
+	  if( user_1 == user_2 ){
+		res.write('{status:error,msg:user_1_and_user_2_eq}');res.end();return;
+	  }
+
+	  
+	  require('mongodb').MongoClient.connect(global.urlMongo, function(err, db) {
+
+		if(err) throw err;
+
+		var collection = db.collection('relationship');
+		  
+		var id= uuid.v1();
+		  
+		collection.insert([{'user':[user_1,user_2],
+							'status':'A',
+							'uuid':id
+				 }], function(err, result) {
+							
+			if(err) throw err;
+
+			 res.write('{status:success,msg:relationship_registered}');res.end();
+			 db.close();
+			 
+		})
+	 
+
+	  })
+ 
+ 
+}
+
+exports.query_profile = function(email) {
+ 
+	
+	
+	require('mongodb').MongoClient.connect(global.urlMongo, function(err, db) {
+		 
+	    var collection = db.collection('user');
+		
+		collection.find( {'email':email} ).limit(1).toArray(function(err, docs) {
+		
+			if(err) throw err;
+			
+			return docs.length;
+			 
+
+			db.close();
+		
+		})
+		   
+		 
+		 
+	})  
+   
+}
+   
