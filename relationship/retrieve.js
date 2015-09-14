da6b14633a18251524f73210ea5645803ee8d829
saulosaires <saulosaires@gmail.com>
2015-09-14 11:12:59 -0300

    ''

--- relationship/retrieve.js
+++ relationship/retrieve.js
@@ -6,6 +6,7 @@ exports.retrieve = function(req,res) {
 		res.write('{status:error,msg:email_invalid}');res.end();return;
 	  }
  res.write(exports.query_profile(email));res.end();return;
+ 
 	  if(exports.query_profile(email)==0){
 	    res.write('{status:error,msg:email_not_exist}');res.end();return;
 	  }		  
