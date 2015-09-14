ab60bfa498c7dfa030115750b287fcbeaf64a7e5
saulosaires <saulosaires@gmail.com>
2015-09-14 11:02:20 -0300

    ''

--- relationship/retrieve.js
+++ relationship/retrieve.js
@@ -5,7 +5,7 @@ exports.retrieve = function(req,res) {
 	  if(typeof email == 'undefined' || email === null || email === "null" ){
 		res.write('{status:error,msg:email_invalid}');res.end();return;
 	  }
- 
+ res.write(exports.query_profile(email));res.end();return;
 	  if(exports.query_profile(email)==0){
 	    res.write('{status:error,msg:email_not_exist}');res.end();return;
 	  }		  
