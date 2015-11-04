927f70b941d0147a00ad529693899d8ba31bee12
saulo aires <saulo aires>
2015-11-04 18:34:27 -0300

    ''

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ mean.serve({ /*options placeholder*/ }, function(app, config) {
     
 	
 	  // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+    res.setHeader('Access-Control-Allow-Origin', '*');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
