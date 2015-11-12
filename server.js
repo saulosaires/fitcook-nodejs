4919597f12118efe2f8bf6d1a58d56bbdcf256f3
saulo aires <saulo aires>
2015-11-12 01:05:47 -0300

    ''

--- server.js
+++ server.js
@@ -20,7 +20,39 @@ mean.serve({ /*options placeholder*/ }, function(app, config) {
   global.urlMongo='mongodb://admin:NlCv6mWKfga1@127.10.56.2:27017/mean';
  
   app.use(cors());
- 
+
+  app.use(function(req, res, next) {
+
+	  // check header or url parameters or post parameters for token
+	  var token = req.body.token || req.query.token || req.headers['x-access-token'];
+
+	  // decode token
+	  if (token) {
+
+		// verifies secret and checks exp
+		jwt.verify(token, app.get('superSecret'), function(err, decoded) {      
+		  if (err) {
+			return res.json({ success: false, message: 'Failed to authenticate token.' });    
+		  } else {
+			// if everything is good, save to request for use in other routes
+			req.decoded = decoded;    
+			next();
+		  }
+		});
+
+	  } else {
+
+		// if there is no token
+		// return an error
+		return res.status(403).send({ 
+			success: false, 
+			message: 'No token provided.' 
+		});
+		
+	  }
+  });
+
+  
   
   app.get('/api/*', function (req,res,next) {
     
