c8fbcbc26921f6d01368c064ca52c346b49ee797
saulo aires <saulo aires>
2015-11-11 23:27:06 -0300

    ''

--- user/index.js
+++ user/index.js
@@ -6,6 +6,4 @@ exports.login = function(req,res) {
 	return user_login.login(req,res);
 }
 
- exports.retrieve = function(req,res) {
-	return user_retrieve.retrieveByEmail(req,res);
-}
+  
--- user/login.js
+++ user/login.js
@@ -27,7 +27,7 @@ exports.login = function(jwt,req,res) {
 			if (docs.length > 0){
 				exports.update_profile(req,res);
 				
-				var token = jwt.sign(user, app.get('superSecret'), {
+				var token = jwt.sign(user, global.secret, {
 				  expiresInMinutes: 1440 // expires in 24 hours
 				});
 				
@@ -54,77 +54,3 @@ exports.login = function(jwt,req,res) {
 	  })
  
 }
-
-
-exports.register_profile = function(req,res) {
-	
-	var name 	 = req.param("name");
-	var photo  = req.param("photo");
-	var email  = req.param("email");
-	var deviceName  = req.param("deviceName");
-    var deviceId  = req.param("deviceId");
-	    
-	require('mongodb').MongoClient.connect(global.urlMongo, function(err, db) {
-		 
-	    var collection = db.collection('user');
-		
-		
-		
-		collection.insert([{'name' :name, 
-							'photo':photo,
-							'email':email,
-							'deviceName':deviceName,
-							'deviceId':deviceId
-						 }], function(err, result) {
-							
-			if(err) throw err;
-
-			 res.write('{status:success,msg:profile_registered}');res.end();
-			 db.close();
-			 
-		})
-		   
-		 
-		 
-	})  
-   
-}	  
-
-exports.update_profile = function(req,res) {
-	
-	var name 	    = req.param("name");
-	var photo       = req.param("photo");
-	var email       = req.param("email");
-	var deviceName  = req.param("deviceName");
-    var deviceId    = req.param("deviceId");
-	    
-	require('mongodb').MongoClient.connect(global.urlMongo, function(err, db) {
-		 
-	    var collection = db.collection('user');
-		 
-		collection.update({'email':email,'deviceId':deviceId} ,
-						   {
-						     'name' :name, 
-							 'photo':photo,
-							 'email':email,
-							 'deviceName':deviceName,
-							 'deviceId':deviceId
-						   }, function(err, result) {
-							
-			if(err) throw err;
-
-			 if(result>0){
-				res.write('{status:success,msg:profile_updated}');res.end();
-			 }else{
-				res.write('{status:fail,msg:none_updated}');res.end();
-			 }
-			 db.close();
-			 
-		})
-		   
-		 
-		 
-	})  
-   
-}		
-
