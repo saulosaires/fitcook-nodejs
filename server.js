5c9377f3c47e343487d7d63c4883a2ef05f7c81b
saulo aires <saulo aires>
2015-11-12 01:14:13 -0300

    ''

--- server.js
+++ server.js
@@ -23,8 +23,9 @@ mean.serve({ /*options placeholder*/ }, function(app, config) {
 
   app.use(function(req, res, next) {
 
+  
 	  // check header or url parameters or post parameters for token
-	  var token = req.body.token || req.query.token || req.headers['x-access-token'];
+	  var token = req.param("deviceId");// req.body.token || req.query.token || req.headers['x-access-token'];
 
 	  // decode token
 	  if (token) {
