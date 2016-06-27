d1dfe8cd45b87ea31ac3023a2bcbd2696bcefdc5
Saulo Aires <saulosaires@gmail.com>
2016-06-26 23:39:11 -0300

    ''

--- recipe/update.js
+++ recipe/update.js
@@ -25,7 +25,7 @@ exports.update = function(req,res) {
 		var collection = db.collection('recipes');
  
 		collection.update({_id:require('mongodb').ObjectID(id)} ,
-							{'ativo':'true','recipe':recipe,'time':new Date().getTime()}, 
+							{'ativo':'true','recipe':JSON.parse(recipe),'time':new Date().getTime()}, 
 							function(err, result) {
 							
 			if(err) throw err;
